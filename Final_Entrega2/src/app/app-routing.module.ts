6d5e8e0e0d584c5e3b8d634cb9f05105c42afa49
Ana Castellanos <lia150687@gmail.com>
2022-10-30 21:28:56 -0300

    mat-sidenav router

--- Final_Entrega2/src/app/app-routing.module.ts
+++ Final_Entrega2/src/app/app-routing.module.ts
@@ -4,10 +4,18 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { DemoCardsComponent } from './cursos/cursos-cards/demo-cards.component';
 import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
 import { MenuLateralComponent } from "./components/menu-lateral/menu-lateral.component";
+import { listaAlumnos } from './models/alumnos';
+import { ListaCursosComponent } from './cursos/components/lista-cursos/lista-cursos.component';
+import { EditarCursoComponent } from './cursos/components/editar-curso/editar-curso.component';
+import { AgregarCursoComponent } from './cursos/components/agregar-curso/agregar-curso.component';
 
 const rutas: Routes = [
     { path: '', redirectTo: 'inicio', pathMatch: "full" },
-    { path: 'cursos', component: DemoCardsComponent },
+    { path: 'cursos', component: DemoCardsComponent, children: [
+        { path:'listar', component: ListaCursosComponent},
+        { path:'editar', component: EditarCursoComponent},
+        { path: 'agregar', component: AgregarCursoComponent}
+    ] },
     { path: 'inicio', component: MenuLateralComponent },
     { path: '**', component: PaginaNoEncontradaComponent}
 ]
